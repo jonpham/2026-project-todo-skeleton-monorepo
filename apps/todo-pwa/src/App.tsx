66a4d6d265a6c11ed329b4c5b25e457fd643dfbe
Jonathan Pham <jonpham@users.noreply.github.com>
2026-04-27 17:43:27 -0700

    Revert "chore: remove empty apps/todo-pwa placeholder (replaced with subtree at apps/todo-pwa-vite)"
    This reverts commit 85f9145f2bc7279c27687eb0056797cd664fa13f.

---
+++ apps/todo-pwa/src/App.tsx
@@ -0,0 +1,11 @@
+import { TodoApp } from "./components/TodoApp";
+
+function App() {
+  return (
+    <main className="mx-auto min-h-screen max-w-xl bg-gray-50 p-8">
+      <TodoApp />
+    </main>
+  );
+}
+
+export default App;
---
+++ apps/todo-pwa/src/components/TodoList/TodoList.tsx
@@ -0,0 +1,38 @@
+import type { TodoItem } from "../../types/todo";
+import { TodoItem as TodoItemComponent } from "../TodoItem";
+
+interface TodoListProps {
+  todos: TodoItem[];
+  onToggle: (id: string) => void;
+  onUpdate: (id: string, description: string) => void;
+  onDelete: (id: string) => void;
+}
+
+export function TodoList({
+  todos,
+  onToggle,
+  onUpdate,
+  onDelete,
+}: TodoListProps) {
+  if (todos.length === 0) {
+    return (
+      <p className="mt-8 text-center text-sm text-gray-400">
+        No to-do items yet. Add one above!
+      </p>
+    );
+  }
+
+  return (
+    <ul className="mt-4 space-y-2">
+      {todos.map((todo) => (
+        <TodoItemComponent
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+        />
+      ))}
+    </ul>
+  );
+}
