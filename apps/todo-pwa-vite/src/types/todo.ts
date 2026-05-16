0ff3b49695c77ada4bcbaf898e1dcd65ac621e85
Jonathan Pham <jonpham@users.noreply.github.com>
2026-05-15 19:08:35 -0700

    feat: define TodoHook interface, SyncStatus, UiTodo in types/todo.ts

--- apps/todo-pwa-vite/src/types/todo.ts
+++ apps/todo-pwa-vite/src/types/todo.ts
@@ -1,1 +1,18 @@
 export type { TodoItem } from "@jonpham/2026-project-todo-types";
+
+export type SyncStatus = "pending" | "synced" | "failed";
+
+export type UiTodo = import("@jonpham/2026-project-todo-types").TodoItem & {
+  syncStatus: SyncStatus;
+};
+
+export interface TodoHook {
+  todos: UiTodo[];
+  createTodo: (description: string) => void;
+  updateTodo: (id: string, description: string) => void;
+  toggleTodo: (id: string) => void;
+  deleteTodo: (id: string) => void;
+  offline: boolean;
+  error: string | null;
+  isLoading: boolean;
+}
